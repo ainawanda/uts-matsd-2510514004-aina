549564be70f321041ac075e67712362d70ab746e
ainawanda <2510514004@mahasiswa.upnvj.ac.id>
2026-04-22 07:57:12 +0700

    soal-1 : benerin variabel personalisasi

---
+++ 02_matlab/soal_1_eksplorasi.m
@@ -0,0 +1,36 @@
+a = 0;
+K = 5;
+b = 4;
+D = zeros(1, 7); 
+for i = 1:7
+    D(i) = 20 + (K * i) + mod(a * i, 7) - mod(b * i, 5);
+end
+
+%verifikasi pakai fungsi MATLAB
+mean_D = mean(D);  
+median_D = median(D);  
+mode_D = mode(D);  
+range_D = range(D);  
+std_D = std(D, 1);
+
+%
+disp('hasil verifikasi MATLAB:');
+disp(['rata-rata (mean): ', num2str(mean_D)]);
+disp(['median: ', num2str(median_D)]);
+disp(['modus : ', num2str(mode_D)]);
+disp(['rentang (range): ', num2str(range_D)]);
+disp(['standar deviasi (Standard Deviation): ', num2str(std_D)]);
+
+%diagram batang penjualan per toko dan garis rata-rata
+figure;  
+bar(D);  
+hold on;  
+yline(mean_D, 'r', 'garis rata-rata', 'LineWidth', 2);
+
+%label dan judul diagram
+xlabel('toko (i)', 'FontSize', 12);
+ylabel('penjualan (dalam juta)', 'FontSize', 12);
+title('diagram batang penjualan rer toko', 'FontSize', 14);
+
+%menampilkan hasil plot
+hold off;
