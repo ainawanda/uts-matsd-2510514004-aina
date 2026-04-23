b39ab464b66afb657393aa27826514aaec04df22
ainawanda <2510514004@mahasiswa.upnvj.ac.id>
2026-04-23 22:21:31 +0700

    soal 4 - menambahkan kode matlab soal 4d

---
+++ 02_matlab/soal_4(d)_kompleks.m
@@ -0,0 +1,33 @@
+% =====================================================
+% UTS Matematika Sains Data — Semester Genap 2025/2026
+% Soal 4(b) - Segmentasi Pelanggan dan komposisi fungsi harga
+% ---------------------------------------------------
+% Nama : Aina Wanda Rahmaliya
+% NIM : 2510514004
+% Parameter: N=4,a=0,b=4,k=5,theta0=20,alpha=0.005
+% Tanggal: 2026-4-23
+% =====================================================
+z1 = 2 + 5i;  
+z2 = 5 - 2i;  
+
+%ez1 + z2
+sum_z = z1 + z2;
+%z1 * z2
+product_z = z1 * z2;
+%z1 / z2
+quotient_z = z1 / z2;
+%|z1 - z2|
+modulus_diff = abs(z1 - z2);
+
+%menampilkan hasil perhitungan
+disp('Hasil z1 + z2:');
+disp(sum_z);
+
+disp('Hasil z1 * z2:');
+disp(product_z);
+
+disp('Hasil z1 / z2:');
+disp(quotient_z);
+
+disp('Modulus dari z1 - z2:');
+disp(modulus_diff);
