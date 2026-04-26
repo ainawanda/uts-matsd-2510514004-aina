ab6231af6eeb591962f0511be4974414bfe5b49b
ainawanda <2510514004@mahasiswa.upnvj.ac.id>
2026-04-26 19:41:02 +0700

    soal 5.1 - merevisi kode matlab no 5.1

--- 02_matlab/soal_5.1_limit_kekontinuan.m
+++ 02_matlab/soal_5.1_limit_kekontinuan.m
@@ -5,22 +5,25 @@
 % Nama : Aina Wanda Rahmaliya
 % NIM : 2510514004
 % Parameter: N=4,a=0,b=4,k=5,theta0=20,alpha=0.005
-% Tanggal: 2026-4-23
+% Tanggal: 2026-4-26
 % =====================================================
 syms x K real
 K = 5;
 
 %L1
 f1 = (x^2 - K^2)/(x - K);
-L1 = limit(f1, x, K)
+L1 = limit(f1, x, K);
+fprintf('Limit L1 (x -> K): %.4f\n', L1)
 
 %L2
 f2 = (sqrt(x + (K+1)^2) - (K+1)) / x;
-L2 = limit(f2, x, 0)
+L2 = limit(f2, x, 0);
+fprintf('Limit L2 (x -> 0): %.4f\n', L2)
 
 %L3
 a = 0;
 b = 4;
 K = 5;
 f3 = ((a+2)*x^3 + b*x + 7) / (x^3 - x + K);
-L3 = limit(f3, x, inf)
+L3 = limit(f3, x, inf);
+fprintf('Limit L3 (x -> inf): %.4f\n', L3)
